aed7dbdb4215defee1fa77a1e67dcb1ef49ddd55
Gary Theilman <gtheilman@gmail.com>
2015-05-30 14:39:32 -0500

    URL Shortening working

--- server/twilio.js
+++ server/twilio.js
@@ -1,82 +1,96 @@
 if (Meteor.isServer) {
     Meteor.methods({
+            shortenURL: function (URL) {
+                var shortenerURL = "http://tiny-url.info/api/v1/random";
+                var queryString = 'url=' + URL;
+                var shortenerURLString = shortenerURL + "?" + queryString;
+                var result = Meteor.http.get(shortenerURLString);
+                return result.content
+            },
 
-        // this is to check if newly submitted credentials are valid
-        'validateTwilioCredentials': function (credentials) {
-            var restURL = "https://api.twilio.com/2010-04-01/Accounts/" + credentials.accountsid + ".json";
-            var auth = credentials.accountsid + ":" + credentials.authtoken;
 
-            var result = Meteor.http.get(restURL,
-                {
-                    auth: auth,
-                    params: {
-                        //  username: credentials.accountsid,
-                        //  password: credentials.authtoken
+            // this is to check if newly submitted credentials are valid
+            'validateTwilioCredentials': function (credentials) {
+                var restURL = "https://api.twilio.com/2010-04-01/Accounts/" + credentials.accountsid + ".json";
+                var auth = credentials.accountsid + ":" + credentials.authtoken;
+
+                var result = Meteor.http.get(restURL,
+                    {
+                        auth: auth,
+                        params: {
+                            //  username: credentials.accountsid,
+                            //  password: credentials.authtoken
+                        }
+                    });
+
+                if (result.statusCode == 200) {
+                    var respJson = JSON.parse(result.content);
+                    if (respJson.sid == credentials.accountsid) {
+                        TwilioCredentials.update(
+                            {},
+                            {
+                                $set: {
+                                    accountsid: credentials.accountsid,
+                                    authtoken: credentials.authtoken,
+                                    createdAt: new Date()
+                                }
+                            }
+                        );
+                    }
+
+                    return respJson.sid;
+                } else {
+                    var errorJson = JSON.parse(result.content);
+                    throw new Meteor.Error(result.statusCode, errorJson.error);
+                }
+            }
+
+            ,
+
+            // this is to check if the credentials already in the db are valid
+            'confirmTwilioCredentials': function () {
+                var credentials = TwilioCredentials.findOne();
+                var restURL = "https://api.twilio.com/2010-04-01/Accounts/" + credentials.accountsid + ".json";
+                var auth = credentials.accountsid + ":" + credentials.authtoken;
+                var result = Meteor.http.get(restURL,
+                    {
+                        auth: auth
+                    });
+
+                if (result.statusCode == 200) {
+                    var respJson = JSON.parse(result.content);
+                    if (respJson.sid == credentials.accountsid) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    return 0
+                }
+            }
+
+            ,
+            'sendSMS': function (to, from, message) {
+                var credentials = TwilioCredentials.findOne();
+                twilio = Twilio(credentials.accountsid, credentials.authtoken);
+                twilio.sendSms({
+                    to: to, // Any number Twilio can deliver to
+                    from: from, // A number you bought from Twilio and can use for outbound communication
+                    body: message // body of the SMS message
+                }, function (err, responseData) { //this function is executed when a response is received from Twilio
+                    if (!err) { // "err" is an error received during the request, if any
+                        // "responseData" is a JavaScript object containing data received from Twilio.
+                        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+                        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+                        console.log(responseData.from); // outputs "+14506667788"
+                        console.log(responseData.body); // outputs "word to your mother."
                     }
                 });
-
-            if (result.statusCode == 200) {
-                var respJson = JSON.parse(result.content);
-                if (respJson.sid == credentials.accountsid) {
-                    TwilioCredentials.update(
-                        {},
-                        {
-                            $set: {
-                                accountsid: credentials.accountsid,
-                                authtoken: credentials.authtoken,
-                                createdAt: new Date()
-                            }
-                        }
-                    );
-                }
-
-                return respJson.sid;
-            } else {
-                var errorJson = JSON.parse(result.content);
-                throw new Meteor.Error(result.statusCode, errorJson.error);
             }
-        },
 
-        // this is to check if the credentials already in the db are valid
-        'confirmTwilioCredentials': function () {
-            var credentials = TwilioCredentials.findOne();
-            var restURL = "https://api.twilio.com/2010-04-01/Accounts/" + credentials.accountsid + ".json";
-            var auth = credentials.accountsid + ":" + credentials.authtoken;
-            var result = Meteor.http.get(restURL,
-                {
-                    auth: auth
-                });
 
-            if (result.statusCode == 200) {
-                var respJson = JSON.parse(result.content);
-                if (respJson.sid == credentials.accountsid) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            } else {
-                return 0
-            }
-        },
-        'sendSMS': function (to, from, message) {
-            var credentials = TwilioCredentials.findOne();
-            twilio = Twilio(credentials.accountsid, credentials.authtoken);
-            twilio.sendSms({
-                to: to, // Any number Twilio can deliver to
-                from: from, // A number you bought from Twilio and can use for outbound communication
-                body: message // body of the SMS message
-            }, function (err, responseData) { //this function is executed when a response is received from Twilio
-                if (!err) { // "err" is an error received during the request, if any
-                    // "responseData" is a JavaScript object containing data received from Twilio.
-                    // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-                    // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-                    console.log(responseData.from); // outputs "+14506667788"
-                    console.log(responseData.body); // outputs "word to your mother."
-                }
-            });
         }
-
-
-    });
+    )
+    ;
 
 }
