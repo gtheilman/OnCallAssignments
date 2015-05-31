02a35bb8a4ba7c9504ab1a419522f57183dfc6eb
Gary Theilman <gtheilman@gmail.com>
2015-05-31 18:58:18 -0500

    Working on responses table.

--- client/views/consults/consults.js
+++ client/views/consults/consults.js
@@ -52,8 +52,6 @@ if (!Meteor.isClient) {
 if (!Meteor.isClient) {
 } else {
     Template.consultForm.helpers({
-
-
         aliceChecked: function () {
             var consult = Consults.findOne({_id: Session.get("consult_id")});
             if (consult.voice == 'alice') {
@@ -73,24 +71,60 @@ if (!Meteor.isClient) {
             if (consult.voice == 'woman') {
                 return consult && "checked"
             }
-        },
-
-        consultResponse: function () {
-            return Responses.find({consult_id: Session.get("consult_id")});
         }
 
 
     });
 
-    Template.responseRow.helpers({
-        responseCells: function () {
-            // return "before";
-            var callInfo = Meteor.call('callInfo', this.callSid);
-            console.log(callInfo.from);
-            return callInfo.from;
+    // Retrieve consult responses from Twilio website and store them in a temporary local collection
+    Template.responses.created = function () {
+        // consult_id was set by iron router when the route was started
+        Responses.find({consult_id: Session.get("consult_id")}).forEach(function (response) {
+            Meteor.call('callInfo', response.callSid, function (err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (response.student_id) {
+                        var student = Students.findOne({_id: response.student_id});
+                    } else {
+                        var student = {};
+                    }
+                    var consult = Consults.findOne({_id: response.consult_id});
 
+                    ConsultResponses.insert({
+                        callSid: data.sid,
+                        to: data.to,
+                        from: data.from,
+                        start_time: data.start_time,
+                        caller_name: data.caller_name,
+                        student_id: response.student_id,
+                        username: student.username,
+                        consult_id: response.consult_id,
+                        shortName: consult.shortName
+
+                    })
+                }
+            });
+        });
+    };
+
+    Template.responses.helpers({
+            consultResponse: function () {
+                return Responses.find({consult_id: Session.get("consult_id")});
+            },
+
+            consultRow: function () {
+                var consultResponse = ConsultResponses.findOne({callSid: this.callSid});
+                var toCell = "<td>" + consultResponse.to + "</td>";
+                var fromCell = "<td>" + consultResponse.from + "</td>";
+                var start_timeCell = "<td>" + consultResponse.start_time + "</td>";
+
+                return toCell;
+
+            }
         }
-    })
+    )
+    ;
 
 
 }
